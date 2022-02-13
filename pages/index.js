42e58908b8edf11d0cbedb7e20c1a228fee62a54
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-02-13 16:49:44 +0200

    React-Overview -> State with Class Component / Hooks

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const List = ({ items }) => (
-  <ul>
-    {items.map((item) => (
-      <li key={item}>{item}</li>
-    ))}
-  </ul>
-)
+const ListFC = ({ items }) => {
+  const [filteredItems, setFilteredItems] = useState(items)
+
+  const handleFilterItems = (e) => {
+    const searchValue = e.target.value
+    const currentItems = [...items]
+    const filteredItems = currentItems.filter((item) =>
+      item.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    setFilteredItems(filteredItems)
+  }
+  return (
+    <>
+      <input onChange={handleFilterItems} />
+      <ul>
+        {filteredItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+class ListCC extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filteredItems: this.props.items
+    }
+    this.handleFilterItems = this.handleFilterItems.bind(this)
+  }
+
+  handleFilterItems(e) {
+    const searchValue = e.target.value
+    const currentItems = [...this.props.items]
+    const filteredItems = currentItems.filter((item) =>
+      item.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    this.setState({ filteredItems })
+  }
+
+  render() {
+    return (
+      <>
+        <input onChange={this.handleFilterItems} />
+        <ul>
+          {this.state.filteredItems.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </>
+    )
+  }
+}
 
 const ListContainer = () => (
-  <List
+  <ListFC
     items={['Learn with M. Elzanaty', 'React JS', 'Next JS', 'Node JS', '??', 'You will get 💰']}
   />
 )
