26982f4b37f5c1ed6adbaf801f6bb65d6339e225
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-02-13 16:22:48 +0200

    React-Overview -> Functional Component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import React from 'react'
 
-class List extends React.Component {
-  render() {
-    return (
-      <ul>
-        <li>Learn with M. Elzanaty</li>
-        <li>React JS</li>
-        <li>Next JS</li>
-        <li>Node JS</li>
-        <li>??</li>
-        <li>You will get 💰💰💰</li>
-      </ul>
-    )
-  }
-}
+const List = () => (
+  <ul>
+    <li>Learn with M. Elzanaty</li>
+    <li>React JS</li>
+    <li>Next JS</li>
+    <li>Node JS</li>
+    <li>??</li>
+    <li>You will get 💰💰💰</li>
+  </ul>
+)
 
 export default List
