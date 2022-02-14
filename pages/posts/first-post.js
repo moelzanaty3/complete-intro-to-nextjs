6eaefb6d994b0bba8b3a5798a3f9fbccf0083611
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-02-14 10:41:08 +0200

    Navigate Between Pages: pages in nextjs

---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+/**
+ * 🐻 Steps:
+ * 1. Create the posts directory under pages.
+ * 2. Create a file called first-post.js inside the posts
+ * 3. The component can have any name, but you must export it as a default export.
+ */
+
+export default function FirstPost() {
+  return <h1>First Post</h1>
+}
+
+/**
+ * 📜 Notes:
+ * Simply create a JS file under the pages directory, and the path to the file becomes the URL path.
+ * In a way, this is similar to building websites using HTML or PHP files.
+ * Instead of writing HTML you write JSX and use React Components.
+ */
