649a0aec9ec6eaa613ed902701647e87eec2d323
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-02-16 19:50:11 +0200

    API routes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// req is an instance of http.IncomingMessage, plus some pre-built middlewares.
+// res is an instance of http.ServerResponse, plus some helper functions.
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
